d1e4cf4a8edbc48ccd5c0f8e3e4cbb1b803a9ae5
valkiboy <val_kik@mail.ru>
2022-04-14 00:30:08 +0300

    написал html, добавил картинки

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,16 @@
 // Swiper 7.4.1
 // import './vendor/swiper';
+
+import './vendor/moveto';
+import MoveTo from './vendor/moveto';
+
+
+const moveTo = new MoveTo({
+  duration: 1000,
+});
+
+const links = document.querySelectorAll('.js-trigger');
+
+links.forEach(function (link) {
+  moveTo.registerTrigger(link);
+});
