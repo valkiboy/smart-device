a52d600b298e6a2e8f89753170a24f588c212d53
valkiboy <val_kik@mail.ru>
2022-04-16 22:26:24 +0300

    Реализовал блок О компании

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,9 @@
 const pageFooter = document.querySelector('.page-footer');
 const accordionItemContent = document.querySelectorAll('.accordion__item-content');
 const accordionToggle = document.querySelectorAll('.accordion__toggle');
+const aboutCompanyHide = document.querySelector('.about-company__hide');
+const aboutCompanyButton = document.querySelector('.about-company__button');
+const aboutCompanySpan = document.querySelector('.about-company span');
 
 // Удаляю  класс "no-js" у футера проверив что элемент присутствует
 
@@ -8,7 +11,7 @@ if (typeof (pageFooter) !== 'undefined' && pageFooter !== null) {
   pageFooter.classList.remove('no-js');
 }
 
-// Добавляю класс "close" всем спискам в аккордеона проверив что элементы присутствуют
+// Скрываю все списки добавляя класс "close"
 
 if (typeof (accordionItemContent) !== 'undefined' && accordionItemContent !== null) {
   accordionItemContent.forEach(function (item) {
@@ -16,7 +19,7 @@ if (typeof (accordionItemContent) !== 'undefined' && accordionItemContent !== nu
   });
 }
 
-// Добавляю класс "toggle--close" всем кнопкам в аккордеоне проверив что элементы есть
+// Добавляю класс "toggle--close" всем кнопкам
 
 if (typeof (accordionToggle) !== 'undefined' && accordionToggle !== null) {
   accordionToggle.forEach(function (item) {
@@ -25,6 +28,8 @@ if (typeof (accordionToggle) !== 'undefined' && accordionToggle !== null) {
 }
 
 const allElems = document.querySelectorAll('.accordion__item-header');
+
+// Вешаю клик на все "шапки" аккордеона для открытия/закрытия элементов
 
 allElems.forEach((elem)=>{
   elem.addEventListener('click', function () {
@@ -45,6 +50,8 @@ allElems.forEach((elem)=>{
       });
     }
 
+    // Открываю скрытый список изменяю вид кнопки
+
     if (typeof (contentBlock && currentToggle) !== 'undefined' && contentBlock && currentToggle !== null) {
       if (contentBlock.classList.contains('close')) {
         contentBlock.classList.remove('close');
@@ -54,3 +61,53 @@ allElems.forEach((elem)=>{
     }
   });
 });
+
+// Функция открытия спрятанного текста
+
+if (typeof (aboutCompanyButton && aboutCompanyHide) !== 'undefined' && aboutCompanyButton && aboutCompanyHide !== null) {
+  aboutCompanyButton.addEventListener('click', function () {
+    const style = aboutCompanyHide.style;
+    style.display = (style.display === 'block') ? 'none' : 'block';
+  });
+}
+
+// Прячу спан если экран меньше 767рх
+
+// if (window.innerWidth < 768) {
+//   if (typeof (aboutCompanyButton && aboutCompanySpan) !== 'undefined' && aboutCompanyButton && aboutCompanySpan !== null) {
+//     aboutCompanyButton.addEventListener('click', function () {
+//       const style = aboutCompanySpan.style;
+//       style.display = (style.display === 'block') ? 'none' : 'block';
+//     });
+//   }
+// }
+
+const changes = (screen) => {
+  if (screen.matches) {
+    if (typeof (aboutCompanyButton && aboutCompanySpan) !== 'undefined' && aboutCompanyButton && aboutCompanySpan !== null) {
+      aboutCompanyButton.addEventListener('click', function () {
+        const style = aboutCompanySpan.style;
+        style.display = (style.display === 'block') ? 'none' : 'block';
+      });
+    }
+  }
+};
+
+if (matchMedia) {
+  const screen = window.matchMedia('(max-width:768px)');
+  screen.addEventListener('change', changes);
+  changes(screen);
+}
+
+// Функция замены надписи на кнопке
+
+if (typeof (aboutCompanyButton) !== 'undefined' && aboutCompanyButton !== null) {
+  aboutCompanyButton.addEventListener('click', function () {
+    if (aboutCompanyButton.innerHTML === 'Подробнее') {
+      aboutCompanyButton.innerHTML = 'Свернуть';
+      return;
+    } else {
+      aboutCompanyButton.innerHTML = 'Подробнее';
+    }
+  });
+}
